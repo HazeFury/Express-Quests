daaf3e56d87171d05777a1f0bf71863b25ae99d9
HazeFury <mab.dev69@gmail.com>
2023-05-24 16:48:36 +0200

    Express 04 quest finished

--- userHandlers.js
+++ userHandlers.js
@@ -49,8 +49,33 @@ const postUser = (req, res) => {
   };
   
 
+  const updateUser = (req, res) => {
+    const id = parseInt(req.params.id);
+    const user = req.body;
+  
+    database
+      .query(
+        "UPDATE users SET ? WHERE id = ?",
+        [user , id]
+      )
+      .then(([result]) => {
+        if (result.affectedRows === 0) {
+          res.status(404).send("Not Found");
+        } else {
+          res.sendStatus(204);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error editing the movie");
+      });
+  };
+
+
+
 module.exports = {
   getUsers,
   getUserById,
   postUser,
+  updateUser,
 };
