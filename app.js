91e165c7fee1302d744fd1dfff04db465e4fcfc0
Marc-Antoine B <115265331+HazeFury@users.noreply.github.com>
2023-06-12 22:51:43 +0200

    Express 08 quest finished

--- app.js
+++ app.js
@@ -14,21 +14,34 @@ app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
 const userHandlers = require("./userHandlers")
-const { hashPassword } = require("./auth.js");
+const login = require("./login")
+const { hashPassword, verifyPassword, verifyToken } = require("./auth");
+
+
+// *********  Public routes  *************
+
+app.post("/api/login",
+  login.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword );
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 
+app.post("/api/users", hashPassword, userHandlers.postUser);
+
+// *********  Protected routes  *************
+app.use(verifyToken);
 
 app.post("/api/movies", movieHandlers.postMovie);
-app.post("/api/users", hashPassword, userHandlers.postUser);
 
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
 
 app.delete("/api/users/:id", userHandlers.deleteUser);
+
+// *******************************************
 
 app.listen(port, (err) => {
   if (err) {
